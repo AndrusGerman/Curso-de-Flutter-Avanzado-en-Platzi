9fd95ec3e09138f0b8a2d1b9ff78e02a72d4d419
AndrusGerman <andrusdiazaleman@gmail.com>
2022-04-29 04:29:15 -0400

    Exponiendo BLoCs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trips_app/platzi_trips.dart';
 
 void main() => runApp(const MyApp());
@@ -7,7 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo', theme: ThemeData(), home: const PlatziTrips());
+    return BlocProvider(
+      bloc: UserBloc(),
+      child: MaterialApp(
+          title: 'Flutter Demo', theme: ThemeData(), home: const PlatziTrips()),
+    );
   }
 }
